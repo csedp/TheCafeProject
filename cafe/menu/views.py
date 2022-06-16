9387d8cb15e3f639eed3f858de8c7feef38a27ae
Rajonya Mondal <rajonyarajjo@gmail.com>
2022-06-16 11:37:55 +0530

    category filtered

--- cafe/menu/views.py
+++ cafe/menu/views.py
@@ -9,7 +9,15 @@ def menu(request):
 def index(request):
     return render(request, 'index.html')
 
-# Product List According to Category
+
 def category_list(request):
     data=Category.objects.all().order_by('-id')
     return render(request,'category_list.html',{'data':data})
+
+# Product List According to Category
+def category_menu_list(request,cat_id):
+	cat=Category.objects.get(id=cat_id)
+	data=Menu.objects.filter(category=cat).order_by('-id')
+	return render(request,'category_menu_list.html',{
+			'data':data,
+			})
