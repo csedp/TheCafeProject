cb34f2d305232c9dc137e1317c06414b854cf30f
Rajonya Mondal <rajonyarajjo@gmail.com>
2022-06-16 12:43:05 +0530

    menu folder structure changed

--- menu/models.py
+++ menu/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     title=models.CharField(max_length=20)
+    def __str__(self) -> str:
+        return self.title
 
 class Menu(models.Model):
     dishName = models.CharField(max_length=20)
@@ -11,3 +13,5 @@ class Menu(models.Model):
     dishPrepTime= models.IntegerField()
     dishImage= models.ImageField(upload_to='menu_imgs/')
     category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return self.dishName
--- menu/urls.py
+++ menu/urls.py
@@ -6,7 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("",views.index, name= "home"),
-    path("menu",views.menu, name= "menu")  
+    path("menu",views.menu, name= "menu"),
+    path("category",views.category_list, name= "category"),
+    path('category-menu-list/<int:cat_id>',views.category_menu_list,name='category-menu-list'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
